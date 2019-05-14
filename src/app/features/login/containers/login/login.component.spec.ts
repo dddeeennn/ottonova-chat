a6087b6d251c3d0b3133d49345ec7e0a3e98f73a
dddeeennn <dess-ase@mail.ru>
2019-05-14 08:59:20 +0300

    add login component

--- src/app/features/login/containers/login/login.component.spec.ts
+++ src/app/features/login/containers/login/login.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { MaterialModule } from '../../../../material/material.module';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,9 +10,13 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [LoginComponent],
+      imports: [
+        MaterialModule,
+        RouterTestingModule.withRoutes([]),
+      ],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
